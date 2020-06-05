a648ae9f9dd35223fd4e6b63500a3e6215eb138a
BuildTools <unconfigured@null.spigotmc.org>
2020-06-05 11:29:34 +0900

    초기 커밋.

---
+++ ConsoleApplication10/ConsoleApplication10.cpp
@@ -0,0 +1,25 @@
+﻿ #include <iostream>
+using namespace std;
+
+int main()
+{
+	int arr[7] = { 1, 4, 3, 3, 5, 7, 6 };
+	int* front = &arr[0];
+	int* back = &arr[6];
+
+	int a = 0;
+	
+
+	for (int i = 0; i <= 2; i++)
+	{
+		a = *(front + i);
+		*(front + i) = *(back - i);
+		*(back - i) = a;
+	}
+
+
+	for (int i = 0; i < 7; i++)
+	{
+		cout << arr[i] << " ";
+	}
+}
